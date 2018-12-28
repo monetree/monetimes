d96702a02e357482045bbfe1ab653527655c93e3
monetree <soubhagyakumar666@gmail.com>
2018-12-28 19:44:48 +0530

    add

--- landing/views.py
+++ landing/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request,'index.html',{})
+
+def work(request):
+    return render(request,'work.html',{})
--- settings/urls.py
+++ settings/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from landing.views import landing
+from landing.views import landing, work
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landing),
+    path('work/', work),
 ]
