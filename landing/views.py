eea45e12e7c992b4248c680d1977a42d21a8fada
soubhagya <soubhagya@recruitmaze.com>
2019-01-02 23:22:38 +0530

    adding changes

--- landing/views.py
+++ landing/views.py
@@ -2,6 +2,3 @@ from django.shortcuts import render
 
 def landing(request):
     return render(request,'index.html',{})
-
-def work(request):
-    return render(request,'work.html',{})
--- settings/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path
-from landing.views import landing, work
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', landing),
-    path('work/', work),
-]
